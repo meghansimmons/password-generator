2d007742ac4215d49344caae7ad22c52cf307010
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-07 23:20:45 -0400

    working password generator

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# password-generator
+# Password Generator
+
+A password generator was created to provide the user with a randomly selected password when a few conditions are met. The password length must be between 8 and 128 characters.  At least one character type must be chosen.  The four character types include special characters, numeric characters and both upper and lowercase letters. After all conditions are met, the password generator displays a randomly generated password. The user can then copy the password to be used as needed. The password generator was created with HTML, CSS and Javascript. 
+
+## Installation
+
+The application is accessible through any web browser at the following address: 
+
+## Usage
+
+The website with a password generator will produce a random password for the user once a suitable length is selected as well as a least one character type (special characters, numbers, lowercase letters and/or uppercase letters).
+
+
+## Contributing
+
+Any information about issues or bugs with the website are appreciated. The current version of the password generator includes starter code given to the developer. A mockup of the finished product, including types of characters to be used and length requirements of the password were also provided to the developer.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -1,95 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";  //set string of special characters
+var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";  //lowercase letters to choose from
+var upperAlphabet = lowerAlphabet.toUpperCase();  //create a string of uppercase letters from the lowercase letter string
+var numberArray = [0,1,2,3,4,5,6,7,8,9]  //create array with numbers
+
+var specialArray = special.split("");   //split string of special characters into an array of special characters
+var lowerArray = lowerAlphabet.split("");  //split lowercase string into an array of lowercase letters
+var upperArray = upperAlphabet.split("");  //split uppercase string into an array of uppercase letters
+
+var typeBooleanArray = []; //array to hold type of character selection answers -true/false
+var chosenCharArray = [];  //array with all chosen characters types (from confirm questions) to chose password from
+var passwordArray = []; //array to hold password of chosen length 
 
 
 function generatePassword(){
-
-var special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var specialChar = special.split("");
-
-var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-var upperAlphabet = lowerAlphabet.toUpperCase();
-var lowerChar = lowerAlphabet.split("");
-var upperChar = upperAlphabet.split("");
-
-var numberChar = [0,1,2,3,4,5,6,7,8,9]
-
-var characterArray = []; //array to hold caracter selection answers
-
-// console.log(specialChar);
-// console.log(numberChar);
-// console.log(lowerChar);
-// console.log(upperChar);
-
-
-
+//Prompt user for length of password. Exit function if conditions are not met
 var passwordLength = prompt("How many characters would you like your password to contain?");
 if (passwordLength >= 8 && passwordLength <= 128) {
-  // console.log(passwordLength);
 } else {
   alert ("The password must be at least 8 characters and at most 128 characters!");
-  console.log("not correct length");
+  return("");
 }
 
-console.log(passwordLength);
-
-// var pass = [];
-// for(var i=0; i < passwordLength; i++ ){
-//   console.log(i);
-  var randomChar = numberChar[Math.floor(Math.random() * numberChar.length)];
-  // console.log(randomChar);
-//   pass.push(randomChar);
-
-// }
-
-console.log(randomChar);
-
+//Confirm with user on which types of characters to include in password.
+//Create an array with answers from the confirm prompt
+//Create an array with the chosen character types
 if (confirm ("Click OK to confirm including special characters.") == true) {
-  characterArray.push(true);
-  console.log("OK for special characters");
+  typeBooleanArray.push(true);
+  chosenCharArray = chosenCharArray.concat(specialArray);
 } else {
-  characterArray.push(false);
-  console.log("NO for special characters");
+  typeBooleanArray.push(false);
 }
 
 if (confirm ("Click OK to confirm including numeric characters.") == true) {
-  characterArray.push(true);
-  console.log("OK for numeric characters");
+  typeBooleanArray.push(true);
+  chosenCharArray = chosenCharArray.concat(numberArray);
 } else {
-  characterArray.push(false);
-  console.log("NO for numeric characters");
+  typeBooleanArray.push(false);
 }
 
 if (confirm ("Click OK to confirm including lowercase characters.") == true) {
-  characterArray.push(true);
-  console.log("OK for lowercase characters");
+  typeBooleanArray.push(true);
+  chosenCharArray = chosenCharArray.concat(lowerArray);
 } else {
-  characterArray.push(false);
-  console.log("NO for lowercase characters");
+  typeBooleanArray.push(false);
 }
 
 if (confirm ("Click OK to confirm including uppercase characters.") == true) {
-  characterArray.push(true);
-  console.log("OK for uppercase characters");
+  typeBooleanArray.push(true);
+  chosenCharArray = chosenCharArray.concat(upperArray);
 } else {
-  characterArray.push(false);
-  console.log("NO for uppercase characters");
+  typeBooleanArray.push(false);
 }
 
-if (characterArray.includes(true)) {
-  console.log("OK for character");
+//Continue to password generation as long as at least one character type was chosen.
+//If no character types were chosen, exit function
+if (typeBooleanArray.includes(true)) {
 } else {
   alert ("Must have at least one character type!");
-  console.log("Must have at least one character type!");
+  return("");
 }
 
 
-
-
-
-return (passwordLength)
+//for loop to choose random password. Both the length and character types are chosen by the user
+for(var i=0; i < passwordLength; i++ ){
+  passwordArray[i] = chosenCharArray[Math.floor(Math.random() * chosenCharArray.length)];  
 }
+
+//change password array to a string for output
+var passwordString = passwordArray.join("");
+
+return (passwordString)
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -98,6 +84,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //clear arrays by setting length to zero
+  passwordArray.length = 0;
+  chosenCharArray.length = 0;
+  typeBooleanArray.length = 0;
 }
 
 // Add event listener to generate button
